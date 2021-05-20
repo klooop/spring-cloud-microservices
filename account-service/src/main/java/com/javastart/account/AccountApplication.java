72bb9faa6cd40f881ab75c638bdbd7f5b6f18eab
Artem Amende <aamende@mail.ru>
2021-05-20 13:15:40 +0300

    test commit

--- account-service/src/main/java/com/javastart/account/AccountApplication.java
+++ account-service/src/main/java/com/javastart/account/AccountApplication.java
@@ -3,5 +3,6 @@ package com.javastart.account;
 public class AccountApplication {
         public static void main(String[] args) {
                 System.out.println("ку");
+                System.out.println("feature-account-service-checkout");
         }
 }
